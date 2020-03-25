26201730b62b58e60326f8c340b3991337528def
kobancheg <aleks.revati@gmail.com>
2020-03-25 14:50:52 +0300

    get tasks localStorage

--- app.js
+++ app.js
@@ -1,35 +1,4 @@
-const tasks = [
-  {
-    _id: "5d2ca9e2e03d40b326596aa7",
-    completed: true,
-    body:
-      "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
-    title: "Eu ea incididunt sunt consectetur fugiat non."
-  },
-  {
-    _id: "5d2ca9e29c8a94095c1288e0",
-    completed: false,
-    body:
-      "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
-    title:
-      "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum."
-  },
-  {
-    _id: "5d2ca9e2e03d40b3232496aa7",
-    completed: true,
-    body:
-      "Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n",
-    title: "Eu ea incididunt sunt consectetur fugiat non."
-  },
-  {
-    _id: "5d2ca9e29c8a94095564788e0",
-    completed: false,
-    body:
-      "Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n",
-    title:
-      "Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum."
-  }
-];
+const tasks = [];
 
 (function(arrOfTasks) {
   const objOfTasks = arrOfTasks.reduce((acc, task) => {
@@ -105,6 +74,9 @@ const tasks = [
   };
 
   let lastSelectedTheme = localStorage.getItem("app_theme") || "default";
+  let renderTasksFromLocal = JSON.parse(localStorage.getItem("objOfTasks"));
+
+  console.log(renderTasksFromLocal);
 
   //   Elements UI
   const listContainer = document.querySelector(
@@ -118,6 +90,7 @@ const tasks = [
   //   Events
   setTheme(lastSelectedTheme);
   renderAllTasks(objOfTasks);
+
   form.addEventListener("submit", onFormSubmitHandler);
   listContainer.addEventListener("click", onDeleteHandler);
   themeSelect.addEventListener("change", onThemeSelectHandler);
@@ -190,6 +163,9 @@ const tasks = [
     };
 
     objOfTasks[newTask._id] = newTask;
+
+    localStorage.setItem("objOfTasks", JSON.stringify(objOfTasks));
+
     return { ...newTask };
   }
 
